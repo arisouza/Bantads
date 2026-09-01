1d501fc675c1b1017c90577d549fedccc36b2d57
Joyce Borecki <joyce-borecki@hotmail.com>
2026-09-01 10:47:26 -0300

    refactor: aplica angular material ao login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, MatFormFieldModule, MatInputModule, MatButtonModule],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
